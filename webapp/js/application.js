79da5ff85d01d66dd9cff81b043b5171ca7650d8
Sven Kellenberger <sven@student-net-hg-0418.ethz.ch>
2019-02-14 15:45:23 +0100

    add percentage functionality

--- webapp/js/application.js
+++ webapp/js/application.js
@@ -4,18 +4,34 @@ var openDonationModal = (function(donation_amount){
   var $form = $modal.find("form");
   $form.empty();
   $form.append("<input type='hidden' id='donation-amount' value='" + donation_amount + "'>")
-  $(".organization-checkbox input:checked").each(function(){
-    var html = "<div class='row'><div class='col s3'>" ;
+  var number_of_charities = $(".organization-checkbox input:checked").length;
+  var ether_per_percentage = donation_amount / 100;
+  var share = Math.floor(100/number_of_charities);
+  $(".organization-checkbox input:checked").each(function(i){
+    var this_share = share;
+    if(i < 100 % number_of_charities) this_share++;
+
+    var html = "<div class='row'><div class='col xl2 s4 valign-wrapper'>" ;
     html += $(this).closest(".card").find(".card-title").text();
-    html += "</div><div class='col s9'><p class='range-field'><input type='range' min='0' max='100'>"
+    html += "</div><div class='col s5 xl8'><p class='range-field'><input type='range' min='0' max='100' value='" + this_share + "'>"
     html += "<span class='thumb'><span class='value'></span></span>";
-    html += "</p></div></div>";
+    html += "</p></div>";
+    html += "<div class='col xl2 s3'><span class='row-amount'>";
+    html += (this_share * ether_per_percentage).toFixed(donation_amount.countDecimals() + 2);
+    html += "</span><img src='./images/ether_logo.png' class='small-ether-logo'></div></div>";
+
     $form.append(html);
   });
 
   var array_of_dom_elements = document.querySelectorAll("input[type=range]");
   M.Range.init(array_of_dom_elements);
   $modal.modal('open');
+});
+
+var updateEtherShare = (function(range){
+  var donation_amount = parseFloat($(range).closest(".modal").find("input#donation-amount").val());
+  var ether_per_percentage = donation_amount / 100;
+  $(range).closest(".row").find(".row-amount").text((ether_per_percentage * $(range).val()).toFixed(donation_amount.countDecimals() + 2));
 });
 
 $(document).ready(function(){
@@ -76,5 +92,71 @@ $(document).ready(function(){
     } else {
       openDonationModal(donation_amount);
     }
+  });
+
+  $("body").on("input", "#donation-modal input[type='range']", function(){
+    $modal = $(this).closest(".modal");
+
+    var $locked = $modal.find("input.locked[type='range']");
+    $(this).addClass("locked");
+
+    if($modal.find("input[type='range']:not(.locked)").length == 0){
+      $modal.find("input[type='range']").removeClass("locked");
+      $(this).addClass("locked");
+    }
+
+    var $unlocked = $modal.find("input[type='range']:not(.locked)");
+
+    var sum_of_ranges = 0;
+    $modal.find("input[type='range']").each(function(){
+      sum_of_ranges += parseInt($(this).val());
+    });
+    console.log(sum_of_ranges);
+
+    var i = -1;
+    while(sum_of_ranges > 100 && $unlocked.length > 0){
+      i = (i+1) % $unlocked.length;
+      $range = $unlocked.eq(i);
+      if(parseInt($range.val()) > 0){
+        $range.val(parseInt($range.val()) - 1);
+        sum_of_ranges--;
+      }
+      $unlocked = $unlocked.filter(function(){
+        return $(this).val() > 0;
+      });
+    }
+
+    i = -1;
+    while(sum_of_ranges > 100 && $locked.length > 0){
+      i = (i+1) % $locked.length;
+      $range = $locked.eq(i);
+      if(parseInt($range.val()) > 0){
+        $range.val(parseInt($range.val()) - 1);
+        sum_of_ranges--;
+      }
+      $locked = $locked.filter(function(){
+        return $(this).val() > 0;
+      });
+    }
+
+    i = -1;
+    while(sum_of_ranges < 100 && $unlocked.length > 0){
+      i = (i+1) % $unlocked.length;
+      $range = $unlocked.eq(i);
+      $range.val(parseInt($range.val()) + 1);
+      sum_of_ranges++;
+    }
+
+    i = -1;
+    while(sum_of_ranges < 100 && $unlocked.length > 0){
+      i = (i+1) % $locked.length;
+      $range = $locked.eq(i);
+      $range.val(parseInt($range.val()) + 1);
+      sum_of_ranges++;
+    }
+
+    $modal.find("input[type='range']").each(function(){
+        updateEtherShare(this);
+    });
   });
 });
